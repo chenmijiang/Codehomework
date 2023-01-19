127818cdbdc6661761db7d23a99523d449f6057a
jack <jack.chenyuana@gmail.com>
2023-01-19 16:51:05 +0800

    framer-motion init

---
+++ React/framer-motion-demo/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function App() {
+  return <Container></Container>
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(0 0 0 / 60%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default App
