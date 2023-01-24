a1e79529a7f05f6500c267281aee740fb6be9b34
jack <jack.chenyuana@gmail.com>
2023-01-24 20:27:23 +0800

    react-router

--- React/lege-management/src/router/index.tsx
+++ React/lege-management/src/router/index.tsx
@@ -1,8 +1,12 @@
-import { lazy } from 'react'
+import React, { lazy } from 'react'
 import { Navigate, useRoutes } from 'react-router-dom'
 
 // import Home from '@/views/Home'
 // import About from '@/views/About'
+import Loading from '@/components/Loading'
+import Page1 from '@/views/Page1'
+const Home = lazy(() => import('@/views/Home'))
+const About = lazy(() => import('@/views/About'))
 
 const baseRouter = () =>
   useRoutes([
@@ -11,12 +15,34 @@ const baseRouter = () =>
       element: <Navigate to="/home" />,
     },
     {
+      path: '/',
+      element: <Home />,
+      children: [
+        {
+          path: '/page1',
+          element: (
+            <Loading>
+              <Page1 />
+            </Loading>
+          ),
+        },
+      ],
+    },
+    {
       path: '/about',
-      element: () => lazy(() => import('@/views/About')),
+      element: (
+        <Loading>
+          <About />
+        </Loading>
+      ),
     },
     {
       path: '/home',
-      element: <Home />,
+      element: (
+        <Loading>
+          <Home />
+        </Loading>
+      ),
     },
   ])
 
